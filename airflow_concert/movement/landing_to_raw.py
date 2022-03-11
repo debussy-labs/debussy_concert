ca75967288a06274c986660474b09a8a6b572125
Nilton Duarte <nilton.gduarte@gmail.com>
2022-03-11 14:31:07 -0300

    refactor - DIP GcsLandingToBigQueryRawMovement

--- airflow_concert/movement/landing_to_raw.py
+++ airflow_concert/movement/landing_to_raw.py
@@ -1,17 +1,9 @@
 from airflow_concert.movement.movement_base import MovementBase
-from airflow_concert.phrase.append_csv import AppendCsvPhrase
-from airflow_concert.phrase.merge_csv import MergeCsvPhrase
-from airflow.utils.task_group import TaskGroup
 
 
 class GcsLandingToBigQueryRawMovement(MovementBase):
-    def __init__(self, config, name=None) -> None:
+
+    def __init__(self, merge_landing_to_raw, name=None) -> None:
+        phrases = [merge_landing_to_raw]
         super().__init__(name=name,
-                         phrases=[AppendCsvPhrase(config), MergeCsvPhrase(config)])
-
-    def build(self, dag, parent_group) -> TaskGroup:
-        task_group = TaskGroup(group_id=self.name, dag=dag, parent_group=parent_group)
-        self.phrases[0].build(dag, task_group)
-        self.phrases[1].build(dag, task_group)
-
-        return task_group
+                         phrases=phrases)
