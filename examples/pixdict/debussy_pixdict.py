986080954dc9c76d19e375e8e164738f0df3298f
Nilton Duarte <nilton.gduarte@gmail.com>
2022-03-11 14:34:04 -0300

    refactoring - updated examples to reflect the changes

--- examples/pixdict/debussy_pixdict.py
+++ examples/pixdict/debussy_pixdict.py
@@ -3,9 +3,12 @@ from airflow_concert.composer.debussy import Debussy
 from airflow.configuration import conf
 
 dags_folder = conf.get('core', 'dags_folder')
-env_file = f'{dags_folder}/pixdict/environment.yaml'
-integration_file = f'{dags_folder}/pixdict/integration.yaml'
+env_file = f'{dags_folder}/examples/pixdict/environment.yaml'
+integration_file = f'{dags_folder}/examples/pixdict/integration.yaml'
 
 debussy = Debussy.crate_from_yaml(environment_yaml_filepath=env_file, integration_yaml_filepath=integration_file)
 mysql = debussy.mysql_composition
-dag = debussy.play(mysql)
+dags = debussy.play(mysql, globals())
+
+# for dag in dags:
+#     globals()[dag.dag_id] = dag
