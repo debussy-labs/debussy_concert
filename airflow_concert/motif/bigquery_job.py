61ede3e75d293a86cb93ed5c304f6662961d19c4
Nilton Duarte <nilton.gduarte@gmail.com>
2022-03-15 19:55:36 -0300

    moved bigquery_to_bigueryto to biggquery_job motif

---
+++ airflow_concert/motif/bigquery_job.py
@@ -0,0 +1,17 @@
+from airflow_concert.motif.motif_base import MotifBase
+from airflow_concert.motif.mixins.bigquery_job import BigQueryJobMixin
+from airflow_concert.phrase.protocols import PExecuteQueryMotif
+from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
+
+
+class BigQueryJobMotif(MotifBase, BigQueryJobMixin, PExecuteQueryMotif):
+    def __init__(self, sql_query, config=None, name=None) -> None:
+        super().__init__(name=name, config=config)
+        self.sql_query = sql_query
+
+    def build(self, dag, task_group):
+        bigquery_job_operator = self.insert_job_operator(
+            dag, task_group,
+            self.query_configuration(sql_query=self.sql_query)
+        )
+        return bigquery_job_operator
