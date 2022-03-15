32f8425e5a975283bace5c79fab4e1253135faa9
Nilton Duarte <nilton.gduarte@gmail.com>
2022-03-14 22:35:53 -0300

    removed vivaldi composition

--- examples/vivaldi_four_seasons/vivaldi_four_seasons_dag.py
+++ examples/vivaldi_four_seasons/vivaldi_four_seasons_dag.py
@@ -1,11 +1,7 @@
 #  from airflow import DAG  # noqa: F401
-from airflow_concert.composition.vivaldi import Vivaldi
+#  from airflow_concert.composition.vivaldi import Vivaldi
 from airflow.configuration import conf
 
 dags_folder = conf.get('core', 'dags_folder')
 env_file = f'{dags_folder}/examples/vivaldi_four_seasons/environment.yaml'
 integration_file = f'{dags_folder}/examples/vivaldi_four_seasons/integration.yaml'
-
-vivaldi = Vivaldi.crate_from_yaml(environment_yaml_filepath=env_file, integration_yaml_filepath=integration_file)
-four_seasons_movement = vivaldi.four_seasons
-dag = vivaldi.play(four_seasons_movement)
