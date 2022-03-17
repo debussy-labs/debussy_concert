1339c7669ac5aa9e6ebd1c2293cdf79d32faa045
Nilton Duarte <nilton.gduarte@gmail.com>
2022-03-17 15:03:25 -0300

    refactoring - added setup to movement for lazy initialization

--- airflow_concert/movement/protocols.py
+++ airflow_concert/movement/protocols.py
@@ -1,5 +1,10 @@
 from typing import Protocol
+from airflow_concert.phrase.protocols import (PExportDataToStorageMotif,
+                                              PCreateExternalTableMotif,
+                                              PMergeTableMotif)
 from airflow_concert.phrase.phrase_base import PPhrase
+from airflow_concert.config.config_integration import ConfigIntegration
+from airflow_concert.entities.table import Table
 #
 # Phrase Protocols
 #
@@ -10,11 +15,19 @@ class PStartPhrase(PPhrase, Protocol):
 
 
 class PIngestionSourceToLandingStoragePhrase(PPhrase, Protocol):
-    pass
+    export_data_to_storage_motif: PExportDataToStorageMotif
+
+    def setup(self, destination_storage_uri):
+        pass
 
 
 class PLandingStorageToDataWarehouseRawPhrase(PPhrase, Protocol):
-    pass
+    create_external_table_motif: PCreateExternalTableMotif
+    merge_table_motif: PMergeTableMotif
+
+    def setup(self, config: ConfigIntegration, table: Table,
+              source_storage_uri_prefix, datawarehouse_raw_uri):
+        pass
 
 
 class PDataWarehouseRawToTrustedPhrase(PPhrase, Protocol):
