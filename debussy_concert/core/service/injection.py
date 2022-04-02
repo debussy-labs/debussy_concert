c1390bbc3665671d8152f8ccd41861f5ed8711ee
Nilton Duarte <nilton.gduarte@gmail.com>
2022-04-01 21:05:26 -0300

    moved injection code to service and updated example

---
+++ debussy_concert/core/service/injection.py
@@ -0,0 +1,12 @@
+from typing import Type
+import inject
+from debussy_concert.core.service.workflow.protocol import PWorkflowService
+from debussy_concert.core.config.config_composition import ConfigComposition
+
+
+def inject_dependencies(workflow_service: PWorkflowService, config_composition: ConfigComposition) -> None:
+    def inject_fn(binder: inject.Binder):
+        binder.bind(PWorkflowService, workflow_service)
+        binder.bind(ConfigComposition, config_composition)
+
+    inject.configure(inject_fn, bind_in_runtime=False)
--- examples/reverse_etl/reverse_etl_dag.py
+++ examples/reverse_etl/reverse_etl_dag.py
@@ -1,28 +1,20 @@
-import inject
 from airflow.configuration import conf
-from debussy_concert.core.config.config_composition import ConfigComposition
 from debussy_concert.reverse_etl.config.reverse_etl import ConfigReverseEtl
 from debussy_concert.reverse_etl.composition.childrens_corner import ChildrensCorner
 from debussy_concert.core.service.workflow.airflow import AirflowService
-from debussy_concert.core.service.workflow.protocol import PWorkflowService
+from debussy_concert.core.service.injection import inject_dependencies, inject_dependencies_from_file
 
 dags_folder = conf.get('core', 'dags_folder')
 env_file = f'{dags_folder}/examples/reverse_etl/environment.yaml'
 composition_file = f'{dags_folder}/examples/reverse_etl/composition.yaml'
 
-composition_config = ConfigReverseEtl.load_from_file(
+reverse_etl_config = ConfigReverseEtl.load_from_file(
     composition_config_file_path=composition_file,
     env_file_path=env_file
 )
 airflow_service = AirflowService()
 
-
-def config_services(binder: inject.Binder):
-    binder.bind(PWorkflowService, airflow_service)
-    binder.bind(ConfigComposition, composition_config)
-
-
-inject.configure(config_services, bind_in_runtime=False)
+inject_dependencies(workflow_service=airflow_service, config_composition=reverse_etl_config)
 
 if __name__ == '__main__':
     import pathlib
