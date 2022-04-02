6008c74956b68c9b3faf84ce4ddef7e08658425f
Nilton Duarte <nilton.gduarte@gmail.com>
2022-04-01 21:06:04 -0300

    added workflow for testing

---
+++ tests/resource/workflow_for_testing.py
@@ -0,0 +1,16 @@
+from debussy_concert.core.service.workflow.protocol import PWorkflowService
+from .core_for_testing import DummyDag, DummyMovementGroup, DummyPhraseGroup, DummyMotifGroup
+
+
+class WorkflowServiceForTesting(PWorkflowService):
+    def motif_group(self, group_id, workflow_dag, phrase_group) -> DummyMotifGroup:
+        return DummyMotifGroup(group_id=group_id, workflow_dag=workflow_dag, phrase_group=phrase_group)
+
+    def phrase_group(self, group_id, workflow_dag, movement_group) -> DummyPhraseGroup:
+        return DummyPhraseGroup(group_id=group_id, workflow_dag=workflow_dag, movement_group=movement_group)
+
+    def movement_group(self, group_id, workflow_dag) -> DummyMovementGroup:
+        return DummyMovementGroup(group_id=group_id, workflow_dag=workflow_dag)
+
+    def workflow_dag(self, dag_id, **kwargs) -> DummyDag:
+        return DummyDag(dag_id=dag_id, **kwargs)
