1c5b2511b34726209dbe065ad09968b5858d25e8
Nilton Duarte <nilton.gduarte@gmail.com>
2022-06-07 12:53:23 -0300

    added CreateOrUpdateBigQueryTableMotif - incomplete implementation, its only creating

---
+++ debussy_concert/data_ingestion/motif/create_update_table_schema.py
@@ -0,0 +1,27 @@
+from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyTableOperator
+from debussy_concert.core.entities.table import Table
+from debussy_concert.core.motif.mixins.bigquery_job import TableReference
+from debussy_concert.core.motif.motif_base import MotifBase
+from debussy_concert.core.service.lakehouse.google_cloud import GoogleCloudLakeHouseService, BigQueryTable
+
+
+class CreateOrUpdateBigQueryTableMotif(MotifBase):
+    def __init__(self, table: BigQueryTable, name=None):
+        super().__init__(name=name)
+        self.bq_schema = GoogleCloudLakeHouseService.get_table_schema(table)
+
+    def setup(self, destination_table_uri):
+        self.destination_table_uri = destination_table_uri
+        self.table_ref = TableReference(table_uri=destination_table_uri)
+
+    def build(self, workflow_dag, phrase_group):
+        create_table = BigQueryCreateEmptyTableOperator(
+            task_id='create_empty_table',
+            project_id=self.table_ref.project_id,
+            dataset_id=self.table_ref.dataset_id,
+            table_id=self.table_ref.table_id,
+            schema_fields=self.bq_schema,
+            dag=workflow_dag,
+            task_group=phrase_group
+        )
+        return create_table
