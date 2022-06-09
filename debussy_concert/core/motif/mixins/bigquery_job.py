a33dee02932654c2ac5f9f2193d6a9388db4304c
Nilton Duarte <nilton.gduarte@gmail.com>
2022-06-09 10:28:49 -0300

    bigquery_job mixin - added .sql to BigQueryInsertJobOperator template_ext and print header options to BigQueryJobMixin

--- debussy_concert/core/motif/mixins/bigquery_job.py
+++ debussy_concert/core/motif/mixins/bigquery_job.py
@@ -6,6 +6,10 @@ from google.cloud.bigquery.external_config import HivePartitioningOptions
 from debussy_concert.core.motif.motif_base import PMotif
 
 
+class BigQueryInsertJobOperator(BigQueryInsertJobOperator):
+    template_ext = (".json", ".sql")
+
+
 class TableReference:
     """
      NOTE: might exist an implementation for this in the google.cloud.bigquery sdk, i could not find it
@@ -88,7 +92,8 @@ class BigQueryJobMixin:
         source_table_uri: str,
         destination_uris: Union[List[str], str],
         field_delimiter: str = ',',
-        destination_format: str = 'CSV'
+        destination_format: str = 'CSV',
+        print_header: bool = True
     ):
         """
             https://cloud.google.com/bigquery/docs/reference/rest/v2/Job#jobconfigurationextract
@@ -122,7 +127,7 @@ class BigQueryJobMixin:
             'extract': {
                 "sourceTable": source_table_ref,
                 "destinationUris": destination_uris,
-                "printHeader": True,
+                "printHeader": print_header,
                 "fieldDelimiter": field_delimiter,
                 "destinationFormat": destination_format
             }
