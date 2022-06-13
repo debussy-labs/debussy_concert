69372dc15513f784a0e10b62276a3bf48c9fa3ce
maycon.silva <maycon.silva@dotz.com>
2022-06-13 10:23:08 -0300

    create RdbmsQueryMotif for invoke StorageToRdbmsOperator

--- debussy_concert/core/motif/rdbms_query.py
+++ debussy_concert/core/motif/rdbms_query.py
@@ -1,6 +1,5 @@
-from typing import Optional
 from debussy_concert.core.motif.motif_base import MotifBase
-from debussy_concert.core.motif.mixins.bigquery_job import BigQueryJobMixin, BigQueryTimePartitioning
+from debussy_concert.core.motif.mixins.bigquery_job import BigQueryJobMixin
 from debussy_concert.core.phrase.protocols import PExecuteQueryMotif
 
 from debussy_framework.v3.hooks.db_api_hook import DbApiInterface
@@ -11,43 +10,32 @@ from debussy_framework.v3.operators.storage_to_rdbms import StorageToRdbmsOperat
 class RdbmsQueryMotif(MotifBase, BigQueryJobMixin, PExecuteQueryMotif):
     destination_table = None
 
-    def __init__(self, name=None,
-                 write_disposition=None,
-                 create_disposition=None,
-                 time_partitioning: Optional[BigQueryTimePartitioning] = None,
-                 gcp_conn_id='google_cloud_default',
+    def __init__(self, 
+                dbapi_hook: DbApiInterface,
+                storage_hook: StorageHookInterface,               
+                name=None,
                  **op_kw_args):
-        #raise Exception(f"time_partitioning: {time_partitioning}")
         super().__init__(name=name)
 
-        self.write_disposition = write_disposition
-        self.time_partitioning = time_partitioning
-        self.create_disposition = create_disposition
-        self.gcp_conn_id = gcp_conn_id
+        self.dbapi_hook = dbapi_hook
+        self.storage_hook = storage_hook
         self.op_kw_args = op_kw_args
 
-    def setup(self, sql_query,
+    def setup(self, bucket_file_path=None,
               destination_table=None):
-        self.sql_query = sql_query
+        self.bucket_file_path = bucket_file_path
         self.destination_table = destination_table
         return self
 
-    def build(self, dag, phrase_group):
-        storage_to_rdbms_operator = StorageToRdbmsOperator(dbapi_hook: DbApiInterface,
-        storage_hook: StorageHookInterface,
-        bucket_file_path)
-        '''
-        self.insert_job_operator(
-            dag, phrase_group,
-            self.query_configuration(
-                sql_query=self.sql_query,
-                destination_table=self.destination_table,
-                create_disposition=self.create_disposition,
-                write_disposition=self.write_disposition,
-                time_partitioning=self.time_partitioning
-            ),
-            self.gcp_conn_id,
+    def build(self, dag, task_group):
+        storage_to_rdbms_operator = StorageToRdbmsOperator(
+            task_id=self.name,
+            dbapi_hook=self.dbapi_hook,
+            storage_hook=self.storage_hook,
+            bucket_file_path=self.bucket_file_path,
+            table_name = self.destination_table,
+            dag=dag, task_group=task_group,
             **self.op_kw_args
-        )
-        '''
+            )
+
         return storage_to_rdbms_operator
