b47fa55700adfba9bb568c2dc216670512c78fc1
Nilton Duarte <nilton.gduarte@gmail.com>
2022-06-22 16:25:03 -0300

    fix - ConfigDataIngestionBase table_prefix correctly returning source_name. this might be deleted in the future

--- debussy_concert/data_ingestion/config/base.py
+++ debussy_concert/data_ingestion/config/base.py
@@ -15,7 +15,7 @@ class ConfigDataIngestionBase(ConfigComposition, ABC):
     source_name: str
     # source tech type like mysql, mssql, bigquery, api, gcs
     source_type: str
-    
+
     @property
     def table_prefix(self):
-        return self.source_type.lower()
+        return self.source_name.lower()
