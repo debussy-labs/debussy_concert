4a3a52cdf00f6b0c819d24f2ab2f048bf55df5e1
Nilton Duarte <nilton.gduarte@gmail.com>
2022-08-15 11:55:11 -0300

    refactoring - table - refactored BigQueryTable, added (BigQuery)TableSchema

--- debussy_concert/core/service/lakehouse/google_cloud.py
+++ debussy_concert/core/service/lakehouse/google_cloud.py
@@ -4,7 +4,7 @@ from debussy_concert.core.entities.table import BigQueryTable
 class GoogleCloudLakeHouseService:
     @staticmethod
     def get_table_schema(table: BigQueryTable):
-        table_fields = table.fields
+        table_fields = table.schema.fields
         table_schema = []
         for field in table_fields:
             table_schema.append(field.get_field_schema())
@@ -17,3 +17,7 @@ class GoogleCloudLakeHouseService:
             partitioning = partitioning.to_dict()
         return partitioning
 
+    @staticmethod
+    def get_table_resource(table: BigQueryTable):
+        print(table.as_dict())
+        return table.as_dict()
