dbc99d6c8b2f5cca5032f027977820f667fa6df0
Lawrence Fernandes <lawrence.fernandes@dotz.com>
2022-09-05 11:42:43 -0300

    Moved config to setup.cfg, formatted through black

--- setup.py
+++ setup.py
@@ -1,15 +1,3 @@
-# -*- coding: utf-8 -*-
+from setuptools import setup
 
-from setuptools import setup, find_packages
-
-setup(
-    name="debussy_concert",
-    version="0.1.0",
-    packages=find_packages(),
-    install_requires=[
-        "Inject",
-        "yaml_env_var_parser",
-        "dbt-bigquery",
-        "airflow-dbt"
-    ]
-)
+setup()
