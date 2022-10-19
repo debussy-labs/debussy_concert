79fd7b83813e5ab0d9607b98b4a4849e5907a50c
maycon.silva <maycon.silva@dotzmkt.com.br>
2022-10-19 12:07:44 -0300

    create ConfigGcsDataIngestion class to load file config yaml

---
+++ debussy_concert/pipeline/data_ingestion/config/gcs_data_ingestion.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass
+from yaml_env_var_parser import load as yaml_load
+
+
+from debussy_concert.core.config.config_environment import ConfigEnvironment
+from debussy_concert.core.config.config_dag_parameters import ConfigDagParameters
+from debussy_concert.pipeline.data_ingestion.config.base import ConfigDataIngestionBase
+from debussy_concert.pipeline.data_ingestion.config.movement_parameters.rdbms_data_ingestion import (
+    RdbmsDataIngestionMovementParameters,
+)
+
+
+@dataclass(frozen=True)
+class ConfigGcsDataIngestion(ConfigDataIngestionBase):
+    secret_manager_uri: str
+
+    @classmethod
+    def load_from_file(cls, composition_config_file_path, env_file_path):
+
+        env_config = ConfigEnvironment.load_from_file(env_file_path)
+
+        with open(composition_config_file_path) as file:
+            config = yaml_load(file)
+        config["environment"] = env_config
+
+        extract_movements = [
+            RdbmsDataIngestionMovementParameters.load_from_dict(parameters)
+            for parameters in config["ingestion_parameters"]
+        ]
+        del config["ingestion_parameters"]
+        config["movements_parameters"] = extract_movements
+        config["dag_parameters"] = ConfigDagParameters.create_from_dict(
+            config["dag_parameters"]
+        )
+        return cls(**config)
