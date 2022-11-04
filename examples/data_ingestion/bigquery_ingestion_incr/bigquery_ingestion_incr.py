8435ce30992c9eaaf0ccb29c98c6bdc6cac49b03
Lawrence Fernandes <lawrence.fernandes@dotz.com>
2022-11-04 12:21:16 -0300

    Update examples to use airflow_easy_setup

--- examples/data_ingestion/bigquery_ingestion_incr/bigquery_ingestion_incr.py
+++ examples/data_ingestion/bigquery_ingestion_incr/bigquery_ingestion_incr.py
@@ -1,10 +1,10 @@
-from debussy_concert.core.config.utils import composition_setup
+from debussy_concert.utils.composition import airflow_easy_setup
 from debussy_concert.pipeline.data_ingestion.composition.bigquery_ingestion import \
     BigQueryIngestionComposition
 from debussy_concert.pipeline.data_ingestion.config.bigquery_data_ingestion import \
     ConfigBigQueryDataIngestion
 
-debussy_composition = composition_setup(
+debussy_composition = airflow_easy_setup(
     rel_path_env_file="examples/environment.yaml",
     rel_path_composition_file="examples/data_ingestion/bigquery_ingestion_incr/composition.yaml",
     CompositionCls=BigQueryIngestionComposition,
--- examples/data_ingestion/mssql_sakila_ingestion/mssql_sakila_ingestion_daily.py
+++ examples/data_ingestion/mssql_sakila_ingestion/mssql_sakila_ingestion_daily.py
@@ -1,19 +1,15 @@
-import os
-
-from debussy_concert.core.config.utils import composition_setup
+from debussy_concert.utils.composition import airflow_easy_setup
 from debussy_concert.pipeline.data_ingestion.composition.rdbms_ingestion import \
     RdbmsIngestionComposition
 from debussy_concert.pipeline.data_ingestion.config.rdbms_data_ingestion import \
     ConfigRdbmsDataIngestion
 
-os.environ["MSSQL_SAKILA_WINDOW_START"] = "execution_date.strftime('%Y-%m-%d 00:00:00')"
-os.environ["MSSQL_SAKILA_WINDOW_END"] = "next_execution_date.strftime('%Y-%m-%d 00:00:00')"
-
-debussy_composition = composition_setup(
+debussy_composition = airflow_easy_setup(
     rel_path_env_file="examples/environment.yaml",
     rel_path_composition_file="examples/data_ingestion/mssql_sakila_ingestion/composition.yaml",
     CompositionCls=RdbmsIngestionComposition,
     CompositionConfigCls=ConfigRdbmsDataIngestion,
+    os_env_prefix='MSSQL_SAKILA'
 )
 
 dag = debussy_composition.auto_play()
