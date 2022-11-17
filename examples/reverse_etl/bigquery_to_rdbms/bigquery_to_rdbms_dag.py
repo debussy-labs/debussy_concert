f40fdaf8b6678b3c6563f7aafc84045859dc0980
Lawrence Fernandes <lawrence.fernandes@dotz.com>
2022-11-17 12:47:30 -0300

    Add bigquery_to_rdbms reverse ETL example

---
+++ examples/reverse_etl/bigquery_to_rdbms/bigquery_to_rdbms_dag.py
@@ -0,0 +1,34 @@
+from airflow.configuration import conf
+from debussy_concert.core.service.injection import inject_dependencies
+from debussy_concert.core.service.workflow.airflow import AirflowService
+from debussy_concert.pipeline.reverse_etl.composition.bigquery_to_rdbms import BigQueryToRdbms
+from debussy_concert.pipeline.reverse_etl.config.reverse_etl import ConfigReverseEtl
+
+dags_folder = conf.get("core", "dags_folder")
+
+env_file = f"{dags_folder}/examples/environment.yaml"
+composition_file = (
+    f"{dags_folder}/examples/reverse_etl/bigquery_to_rdbms/composition.yaml"
+)
+
+reverse_etl_config = ConfigReverseEtl.load_from_file(
+    composition_config_file_path=composition_file, env_file_path=env_file
+)
+
+airflow_service = AirflowService()
+
+inject_dependencies(
+    workflow_service=airflow_service, config_composition=reverse_etl_config
+)
+
+if __name__ == "__main__":
+    import pathlib
+
+    path = pathlib.Path(__file__).parent.resolve()
+    env_file = f"{path}/environment.yaml"
+    composition_file = f"{path}/composition.yaml"
+
+composition = BigQueryToRdbms()
+reverse_etl_movement_fn = composition.bigquery_to_rdbms_reverse_etl_movement_builder
+
+dag = composition.play(reverse_etl_movement_fn)
